c08d67c19338e2f1f3c0812fd5f10345f1ad71b2
Cara Morelli <caramorelli7@gmail.com>
2018-10-12 17:24:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,12 +12,12 @@ class Student
     @first_name
   end 
   
-  def last_name=(first_name)
-    @first_name = first_name
+  def last_name=(last_name)
+    @last_name = last_name
   end 
   
-  def first_name
-    @first_name
+  def last_name
+    @last_name
   end 
 
 end
